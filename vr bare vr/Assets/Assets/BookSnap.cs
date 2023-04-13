44f3bb95efbbc3780fa6d900676299bf0c64c888
tobi291a <tobi291a@videndjurs.net>
2023-04-13 08:52:00 +0200

    hh

---
+++ vr bare vr/Assets/Assets/BookSnap.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookSnap : MonoBehaviour
+{
+    public GameObject book;
+    private bool snapped = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == book)
+        {
+            snapped = true;
+            book.transform.position = transform.position;
+           
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == book)
+        {
+            snapped = false;
+        }
+    }
+
+    public bool IsSnapped()
+    {
+        return snapped;
+    }
+
+    public void DetachBook()
+    {
+        snapped = false;
+    }
+}
