c750595bf5bfe1f20e16eefbc458ab163e25e3ba
ScandieFox <Nikolai.foxdal@gmail.com>
2023-04-19 11:46:59 +0200

    book ok
    jjj

---
+++ vr bare vr/Assets/Assets/BookshelfPuzzle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookshelfPuzzle : MonoBehaviour
+{
+    public BookSnap bookSnapScript; // Reference to the BookSnap script
+    public Transform[] bookSlots; // Array of book slots representing the correct order
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
